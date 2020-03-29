193d3ea929dd358d037444eb3bffa4a4eb950b8b
phlinhng <phlinhng@outlook.com>
2020-03-30 00:22:51 +0800

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+apt-get install curl git -y
 git clone https://github.com/phlinhng/docker-caddy-v2ray-cf.git
-#curl -sSL https://get.docker.com/ | sh
-#sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
+curl -sSL https://get.docker.com/ | sh
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 cd docker-caddy-v2ray-cf
 
@@ -19,11 +20,13 @@ sed -i "" "s/FAKEPATH/$path/g" ./src/caddy/Caddyfile
 sed -i "" "s/FAKEEMAIL/$cfemail/g" docker-compose.yml
 sed -i "" "s/FAKEKEY/$cfapikey/g" docker-compose.yml
 
-#docker-compose up --build -d
+docker-compose up --build -d
 
+echo ""
 echo "Address: $domain"
 echo "Port: 443"
 echo "UUID: $uuid"
+echo "Alter ID: 36"
 echo "Type: websocket"
 echo "Hostname: $domain"
 echo "Path: /$path"

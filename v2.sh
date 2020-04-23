9dbe2ba4d91d590c7fee455a465384d146c8af10
phlinhng <phlinhng@outlook.com>
2020-04-23 10:55:18 +0800

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -10,7 +10,7 @@ V2_PATH=$2
 CF_EMAIL=$3
 CF_APIKEY=$4
 
-apt-get install curl git uuid-runtime coreutils libcap2-bin -y
+apt-get install curl git uuid-runtime coreutils libcap2-bin wget -y
 
 # install v2ray
 bash <(curl -L -s https://install.direct/go.sh)
@@ -61,6 +61,9 @@ chmod 644 /etc/caddy/Caddyfile
 /bin/cp caddy.service /etc/systemd/system/caddy.service
 chown root:root /etc/systemd/system/caddy.service
 chmod 644 /etc/systemd/system/caddy.service
+
+(crontab -l 2>/dev/null; echo "0 7 * * * wget -o /usr/bin/v2ray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/blob/release/geoip.dat?raw=true >/dev/null >/dev/null") | crontab -
+(crontab -l 2>/dev/null; echo "0 7 * * * wget -o /usr/bin/v2ray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/blob/release/geosite.dat?raw=true >/dev/null >/dev/null") | crontab -
 
 systemctl daemon-reload
 
